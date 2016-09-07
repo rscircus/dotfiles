a673e2fa7e5e81eeaa47fda6f48afcc83526d717
Roland Siegbert <roland@siegbert.info>
2016-09-07 17:01:17 +0200

    bootstrap: Fix ZSH_CUSTOM installation errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,11 +41,11 @@ echo "Installing oh-my-zsh..."
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 #TODO: copy rawland theme to .oh-my-zsh
-#
+
 # Install various zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
 
 ####
 echo "Installing diff-highlight, ack and tpm for tmux..."
