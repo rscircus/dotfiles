87f43024b7021d8dbcd8295e62c489919a1a20b0
Roland Siegbert <roland@siegbert.info>
2019-06-08 21:41:21 +0200

    bootstrap: Update to homebrew config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #TODO: Input such that the user doesn't destroy his/her existing config files
 
-# Create local *nix-ish filesystem:
+####
+echo "Create local *nix-ish filesystem..."
+####
+#
 mkdir -p ~/local/bin
-mkdir -p ~/local/share
-mkdir -p ~/local/include
-mkdir -p ~/local/src
-
+# TODO: Move to ~/.local and adapt ~/.zshrc
 
 ####
-echo "Executables..."
+echo "Executables get copied to ~/local/bin..."
 ####
 
 # Copy ~/dotfiles/bin to ~/local/bin
@@ -22,6 +22,7 @@ echo "Installing oh-my-zsh..."
 
 # Install oh-my-zsh:
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# TODO: This interrupts this script. Maybe a silent way is possible
 
 # Copy my theme into theme dir
 cp ~/dotfiles/zsh/themes/rawland.zsh-theme ~/.oh-my-zsh/themes
@@ -31,13 +32,13 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-
 git clone git://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
 
-
 ####
-echo "Installing fzf..."
+echo "Install linux/homebrew..."
 ####
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
+git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+mkdir ~/.linuxbrew/bin
+ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
+eval $(~/.linuxbrew/bin/brew shellenv)
 
 ####
 echo "Installing tpm for tmux..."
@@ -54,68 +55,29 @@ echo "Installing driller..."
 
 wget https://raw.githubusercontent.com/jonhiggs/driller/latest/driller -O ~/local/bin/driller
 chmod +x ~/local/bin/driller
-####
-echo "Installing marktag, doing & maid..."
-####
 
 
 ####
-echo "Installing ripgrep, fd-find, exa, bat..."
-####
-cargo install ripgrep       # faster grepping
-cargo install fd-find       # find things
-cargo install exa           # ls replacement
-cargo install bat           # cat replacement
-
-
-####
-echo "Installing desk..."
-####
-curl https://raw.githubusercontent.com/jamesob/desk/master/desk > ~/local/bin/desk
-# oh-my-zsh
-cd ~/.oh-my-zsh/custom/plugins
-git clone git@github.com:jamesob/desk.git /tmp/desk && cp -r /tmp/desk/shell_plugins/zsh desk
-
-
-####
-echo "Installing thefuck, asciinema, visidata, youtube-dl..."
+echo "Installing many practical bins via brew..."
 ####
 
-brew install thefuck
-brew install youtube-dl
+brew install diff-so-fancy        # fancy diffs in git and other tools
+brew install prettyping           # beautiful ping
+brew install fd			  # alternative to find, which does what one assumes
+brew install ripgrep              # fast grepping
+brew install bat		  # alternative to cat
+brew install exa                  # modern ls
+brew install fzf                  # fuzzy finder
+brew install fasd                 # quick access to files and directories
+brew install thefuck              # I made a mistake, correct it
+brew install tldr		  # tldr; alternative to man
+brew install youtube-dl		  # process youtube videos
+brew install ncdu		  # disk usage analyzer
 #pip install --user asciinema
 #pip install --user visidata
 
 
-####
-echo "Installing tasklib (Python Taskwarrior)..."
-####
-
-pip install --user --upgrade git+git://github.com/tbabej/tasklib@develop
-
-
-####
-echo "Installing TaskWarrior..."
-####
-git clone https://git.tasktools.org/scm/tm/task.git ~/dotfiles/utils/taskwarrior
-cd ~/dotfiles/utils/taskwarrior
-cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=~/.local .
-make
-make install
-
-
-####
-echo "Installing fasd..."
-####
-git clone https://github.com/clvv/fasd ~/local/share/fasd
-cp ~/local/share/fasd/fasd ~/local/bin/fasd
-eval "$(fasd --init auto)"s
-
-
-####
-echo "Installing oh-my-git..."
-####
-git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh → ~/.zshrc
+# TODO:
 ####
 echo "git-time-metric is nice, but doesn't install well..."
 echo "Check: https://github.com/git-time-metric/gtm ..."
@@ -128,6 +90,8 @@ echo "Linking rc files..."
 
 # Configuration files:
 ######################
+# TODO: Check if not overwriting files:
+#
 #ln -sf ~/dotfiles/config/bashrc ~/.bashrc
 ln -sf ~/dotfiles/config/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/config/ctags ~/.ctags
@@ -140,21 +104,22 @@ ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 
 
 ####
-echo "Installing hosts-gen...(sudo required)"
+echo "Installing hosts based adblocking: hosts-gen...(sudo required)"
 ####
-cd ~/local/src
-git clone http://git.r-36.net/hosts-gen
-cd hosts-gen
-sudo make install
-sudo hosts-gen
-
-# Now that gets you a few blockable domains:
-sudo cp examples/gethostszero /bin
-sudo chmod 775 /bin/gethostszero
-sudo /bin/gethostszero
-sudo hosts-gen
-
-# Update it from time to time:
-echo "Add this to root's crontab -e:"
-echo "@weekly gethostszero"
-echo "@weekly hosts-gen"
+# TODO: This moved, have to paths
+# cd ~/local/src
+# git clone http://git.r-36.net/hosts-gen
+# cd hosts-gen
+# sudo make install
+# sudo hosts-gen
+#
+# # Now that gets you a few blockable domains:
+# sudo cp examples/gethostszero /bin
+# sudo chmod 775 /bin/gethostszero
+# sudo /bin/gethostszero
+# sudo hosts-gen
+#
+# # Update it from time to time:
+# echo "Add this to root's crontab -e:"
+# echo "@weekly gethostszero"
+# echo "@weekly hosts-gen"
