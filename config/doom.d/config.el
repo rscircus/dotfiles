82cd37ff5501267957217766efea0dc1ce0e8db4
Roland Siegbert <roland@siegbert.info>
2019-06-13 23:59:25 +0200

    emacs: Update configs to match org extensions

--- config/doom.d/config.el
+++ config/doom.d/config.el
@@ -6,8 +6,17 @@
  doom-big-font (font-spec :family "Iosevka Term Medium" :size 17)
  doom-variable-pitch-font (font-spec :family "Input" :size 16))
 
+;; TODO
+;;;; (when IS-LINUX
+;;;;   (font-put doom-font :weight 'semi-light))
+;;;; (when IS-MAC
+;;;;   (setq ns-use-thin-smoothing t))
+
 ;; Themes
 (require 'doom-themes)
+
+;;; Frames/Windows
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
 
 ;; Global settings (defaults)
 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
@@ -15,13 +24,14 @@
 
 ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
 ;; may have their own settings.
-(load-theme 'doom-peacock' t)
+(load-theme 'doom-one-light' t)
 
 ;; Enable flashing mode-line on errors
 (doom-themes-visual-bell-config)
 
 ;; Enable custom neotree theme (all-the-icons must be installed!)
 (doom-themes-neotree-config)
+
 ;; or for treemacs users
 (doom-themes-treemacs-config)
 
@@ -30,3 +40,71 @@
 
 ;; Enable emoji globally
 (add-hook 'after-init-hook #'global-emojify-mode)
+
+
+;;; :lang org
+(after! org
+  (add-to-list 'org-modules 'org-habit t))
+(setq org-directory "~/work/org/"
+      org-agenda-files (list org-directory)
+      org-ellipsis " ▼ "
+
+      ;; The standard unicode characters are usually misaligned depending on the
+      ;; font. This bugs me. Markdown #-marks for headlines are more elegant.
+      org-bullets-bullet-list '("#"))
+
+
+;;
+;;; Keybinds
+
+(map! :m "M-j" #'multi-next-line
+      :m "M-k" #'multi-previous-line
+
+      ;; Easier window movement
+      :n "C-h" #'evil-window-left
+      :n "C-j" #'evil-window-down
+      :n "C-k" #'evil-window-up
+      :n "C-l" #'evil-window-right
+
+      (:map vterm-mode-map
+        ;; Easier window movement
+        :i "C-h" #'evil-window-left
+        :i "C-j" #'evil-window-down
+        :i "C-k" #'evil-window-up
+        :i "C-l" #'evil-window-right)
+
+      (:map evil-treemacs-state-map
+        "C-h" #'evil-window-left
+        "C-l" #'evil-window-right
+        "M-j" #'multi-next-line
+        "M-k" #'multi-previous-line)
+
+      (:when IS-LINUX
+        "s-x" #'execute-extended-command
+        "s-;" #'eval-expression
+        ;; use super for window/frame navigation/manipulation
+        "s-w" #'delete-window
+        "s-W" #'delete-frame
+        "s-n" #'+default/new-buffer
+        "s-N" #'make-frame
+        "s-q" (if (daemonp) #'delete-frame #'evil-quit-all)
+        ;; Restore OS undo, save, copy, & paste keys (without cua-mode, because
+        ;; it imposes some other functionality and overhead we don't need)
+        "s-z" #'undo
+        "s-c" (if (featurep 'evil) #'evil-yank #'copy-region-as-kill)
+        "s-v" #'yank
+        "s-s" #'save-buffer
+        ;; Buffer-local font scaling
+        "s-+" #'doom/reset-font-size
+        "s-=" #'doom/increase-font-size
+        "s--" #'doom/decrease-font-size
+        ;; Conventional text-editing keys
+        "s-a" #'mark-whole-buffer
+        :gi [s-return]    #'+default/newline-below
+        :gi [s-S-return]  #'+default/newline-above
+        :gi [s-backspace] #'doom/backward-kill-to-bol-and-indent)
+
+      :leader
+      (:prefix "f"
+        "t" #'find-in-dotfiles
+        "T" #'browse-dotfiles))
--- config/doom.d/packages.el
+++ config/doom.d/packages.el
@@ -6,3 +6,5 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
   (package! emojify)
+  (package! org-pomodoro)
+  (package! org-noter)
