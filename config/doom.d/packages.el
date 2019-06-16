d35c3b248ae79ab7e54d78077e7a64b74fcfd3b6
Roland Siegbert <roland@siegbert.info>
2019-06-17 00:29:11 +0200

    emacs: Add org and eshell packages

--- config/doom.d/packages.el
+++ config/doom.d/packages.el
@@ -1,10 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;;; Examples:
-; (package! some-package)
-;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
-;; (package! builtin-package :disable t)
-  (package! emojify)
-  (package! org-pomodoro)
-  (package! org-noter)
+;; writing and org
+(package! emojify)
+(package! org-pomodoro)
+(package! org-noter)
+(package! writeroom-mode)
+
+;; shell
+(package! esh-autosuggest)
+(package! fish-completion)
